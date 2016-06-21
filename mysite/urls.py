51b313f3f5ef3ee21eec0a516101ba58820f473f
Kevin <nivekarod@gmail.com>
2016-06-21 10:44:23 +0800

    mga bago

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 import django.contrib.auth.views
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = [
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
 	url(r'^accounts/logout/$', django.contrib.auth.views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
-]
+)
